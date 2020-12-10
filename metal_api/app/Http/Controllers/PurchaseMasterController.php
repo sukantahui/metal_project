8aa03faf1a3c443bb65174eef2555c305bfef606
Sukanta Hui <huisukanta77@gmail.com>
2020-12-10 20:13:50 +0530

    updated

---
+++ metal_api/app/Http/Controllers/PurchaseMasterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseMaster;
+use Illuminate\Http\Request;
+
+class PurchaseMasterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PurchaseMaster  $purchaseMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PurchaseMaster $purchaseMaster)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PurchaseMaster  $purchaseMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PurchaseMaster $purchaseMaster)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PurchaseMaster  $purchaseMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PurchaseMaster $purchaseMaster)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PurchaseMaster  $purchaseMaster
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PurchaseMaster $purchaseMaster)
+    {
+        //
+    }
+}
