ab83ff6c0225e5b0a36d23dda20e8fbd858f669f
sreeparnadas <sreeparnadas0675@gmail.com>
2021-01-02 22:21:14 +0530

    purchase form modified

--- metal_api/database/migrations/2020_12_10_063409_create_purchase_masters_table.php
+++ metal_api/database/migrations/2020_12_10_063409_create_purchase_masters_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Carbon;
 
 class CreatePurchaseMastersTable extends Migration
 {
@@ -15,9 +16,9 @@ class CreatePurchaseMastersTable extends Migration
     {
         Schema::create('purchase_masters', function (Blueprint $table) {
             $table->id();
-            $table->string('invoice_number',20)->nullable(false);
+            $table->string('invoice_number',20)->nullable(true);
             $table->string('case_number',20)->nullable(true);
-            $table->date('order_date')->useCurrent();
+            $table->date('order_date')->nullable(true);
             $table->string('comment',255)->nullable(true);
 
             $table->tinyInteger('inforce')->default('1');
